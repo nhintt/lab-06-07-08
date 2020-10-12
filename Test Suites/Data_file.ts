f6eea8776b77d54b4a1060e0698efc17008dce1b
nhintt <nhintt2411@gmail.com>
2020-10-12 22:42:54 +0700

    lab/6/7/8

---
+++ Test Suites/Data_file.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Data_file</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>02266a9a-b051-4cab-8b56-6d5edde39822</testSuiteGuid>
+   <testCaseLink>
+      <guid>b125d77a-58ba-49cf-a7c5-c5e027502165</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_data_file</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>698d5b0a-5a70-4665-99f9-89dcedfa46a6</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/TDT_001</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>698d5b0a-5a70-4665-99f9-89dcedfa46a6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>8ba57f57-7320-4bf3-b697-4ce79bf61c84</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>698d5b0a-5a70-4665-99f9-89dcedfa46a6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>42858444-a812-498e-a5ed-cb8aaf0e1197</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
